d536ea3363871aec48e62660e4cd7c3591adced2
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-22 22:00:30 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    casa = models.IntegerField(null=True, blank=True)
+    carro = models.IntegerField(null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Carro(models.Model):
+
+    #__Carro_FIELDS__
+    marca = models.TextField(max_length=255, null=True, blank=True)
+    casas = models.IntegerField(null=True, blank=True)
+
+    #__Carro_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Carro")
+        verbose_name_plural = _("Carro")
+
+
+class Casa(models.Model):
+
+    #__Casa_FIELDS__
+    cuartos = models.IntegerField(null=True, blank=True)
+    baños = models.IntegerField(null=True, blank=True)
+    metros2 = models.IntegerField(null=True, blank=True)
+
+    #__Casa_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Casa")
+        verbose_name_plural = _("Casa")
+
+
+
+#__MODELS__END
